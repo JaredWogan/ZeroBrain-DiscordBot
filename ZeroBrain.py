6157cde6c78c4127ea5be95778b8407a8f2d29f2
Jared Wogan <jared.wogan@gmail.com>
2021-05-21 19:42:34 -0400

    Updated version of ZeroBrain Discord Bot

--- ZeroBrain.py
+++ ZeroBrain.py
@@ -5,6 +5,7 @@
 import asyncio
 import os
 import random
+import wikipedia
 
 import discord
 from discord.ext import tasks
@@ -18,20 +19,32 @@ intents = discord.Intents().all()
 intents.members = True
 client = discord.Client(intents=intents)
 
+zach_id = 0
+
 
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
-    channel_general_id = await getChannelGeneral()
-    print('The General channel has ID:', channel_general_id)
-
     guild = discord.utils.get(client.guilds, name=GUILD)
     members_data = [member for member in guild.members]
-    members = [[member.name.encode('cp850', errors='replace').decode('cp850'), member.id] for member in members_data]
-    print('Members:\n', '-', '\n - '.join(['Name: ' + member[0] + ' - ID: ' + str(member[1]) for member in members]))
+    members = [
+        [member.name.encode('cp850', errors='replace').decode('cp850'), member.id,
+         nicknames(member.nick), voice(member)]
+        for member in members_data
+    ]
+    users_info = [
+        'Name: ' + member[0] + ' - ID: ' + str(member[1]) + ' - Nick: ' + member[2] + ' - Channel: ' + member[3]
+        for member in members
+    ]
+    print('Members:\n', '-', '\n - '.join(users_info))
 
-    drink.start(channel_general_id)
+    zachs = [member for member in members_data if 'Zach' in nicknames(member.nick)]
+    zach = zachs[0]
+
+    drink.start(getChannelGeneral())
+    zachFat.start(getChannelGeneral(), zach)
+    random_page.start(getChannelGeneral())
 
 
 @client.event
@@ -39,15 +52,36 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    response = 'Zach Fat!'
-    await message.channel.send(response)
-
-    # if message.content == '':
-    #     response = 'Zach Fat!'
-    #     await message.channel.send(response)
-
     if message.content == 'shutdown':
         exit()
+
+    if 'http' not in message.content and '!clear' not in message.content:
+        with open('Markov.txt', 'a', encoding='utf-8') as file:
+            file.write(str(message.content).encode('cp850', errors='replace').decode('cp850') + '\n')
+
+    messages = [
+        '(⓿_⓿)',
+        'w(ﾟДﾟ)w',
+        '(╯‵□′)╯︵┻━┻',
+        'https://tenor.com/view/blank-stare-really-i-dont-believe-you-side-eye-looking-gif-6151149',
+        'https://tenor.com/view/thomas-the-tank-engine-shocked-surprised-woah-what-gif-5667771'
+    ]
+    chance = 5
+    if message.channel.id == getChannelGeneral():
+        if random.randint(0, 100) <= chance:
+            response = random.choice(messages)
+            await message.channel.send(response)
+
+
+@tasks.loop(seconds=30)
+async def zachFat(channel_id, zach):
+    msg_channel = client.get_channel(channel_id)
+    zach_connected = [True if voice(zach) != 'Not Connected' else False, False]
+    while True:
+        if zach_connected[0] and not zach_connected[1]:
+            await msg_channel.send(f'<@{zach.id}> Zach Fat ')
+        zach_connected = [True if voice(zach) != 'Not Connected' else False, zach_connected[0]]
+        await asyncio.sleep(1)
 
 
 @tasks.loop(seconds=30)
@@ -68,10 +102,50 @@ async def drink(channel_id):
         await msg_channel.send('🍻 Finish your drink 🍻')
 
 
-async def getChannelGeneral():
+@tasks.loop(seconds=30)
+async def random_page(channel_id):
+    msg_channel = client.get_channel(channel_id)
+    while True:
+        sleepTimer = random.randint(6000, 43200)
+        print(
+            'Sending Wikipedia fact in',
+            sleepTimer // 3600,
+            'hour(s)',
+            sleepTimer % 3600 // 60,
+            'minute(s) and',
+            (sleepTimer - 3600 * (sleepTimer // 3600) - 60 * (sleepTimer % 3600 // 60)),
+            'seconds'
+        )
+        await asyncio.sleep(sleepTimer)
+        random_page = wikipedia.random(1)
+        try:
+            result = wikipedia.page(random_page).summary
+            await msg_channel.send(result)
+        except wikipedia.exceptions.DisambiguationError:
+            print('Wikipedia Error')
+
+
+def getChannelGeneral():
     guild = discord.utils.get(client.guilds, name=GUILD)
     channels = [channel for channel in guild.text_channels]
     channel_general_ids = [channel.id for channel in channels if 'general' in channel.name]
     return channel_general_ids[0]
+
+
+def voice(member):
+    if member is None:
+        return 'Error'
+    if member.voice is None:
+        return 'Not Connected'
+    else:
+        return str(member.voice.channel).encode('cp850', errors='replace').decode('cp850')
+
+
+def nicknames(nickname):
+    if nickname is None:
+        return 'No Nickname'
+    else:
+        return str(nickname)
+
 
 client.run(TOKEN)
